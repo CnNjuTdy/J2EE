9f80d2a3d724cf7f2d58b0f1b2a5ced99915f94a
Tondiyee <tdy_nju@163.com>
2017-01-01 17:52:05 +0800

    commit

---
+++ Homework04/src/main/java/servlets/ExamResultServlet.java
@@ -0,0 +1,69 @@
+package servlets;
+
+import action.bussiness.ExamListBean;
+import action.bussiness.OnlineBean;
+import factory.ServiceFactory;
+import services.ExamService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/exam.do")
+public class ExamResultServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    public ExamService service = null;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExamResultServlet() {
+        super();
+    }
+
+    public void init() {
+        service = ServiceFactory.getExamService();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session == null) {
+            resp.sendRedirect("login.do");
+        } else {
+            String username = (String) session.getAttribute("loginUser");
+            if (username == null) {
+                resp.sendRedirect("login.do");
+            } else {
+                RequestDispatcher dispatcher = req.getRequestDispatcher("/jsp/exam.jsp");
+                // TODO: 2016/12/29 设置两个bean属性
+                ExamListBean examListBean = new ExamListBean();
+                examListBean.setExamList(ServiceFactory.getExamService().getExamList(username));
+                session.setAttribute("examList",examListBean);
+
+                OnlineBean onlineBean = new OnlineBean();
+                onlineBean.setLoginNumber((Integer) getServletContext().getAttribute("online"));
+                onlineBean.setVisitNumber((Integer) getServletContext().getAttribute("offline"));
+                session.setAttribute("onlineNumber",onlineBean);
+
+                session.setAttribute("username",username);
+                if (dispatcher != null) {
+                    dispatcher.include(req, resp);
+                }
+            }
+        }
+
+    }
+
+
+}
